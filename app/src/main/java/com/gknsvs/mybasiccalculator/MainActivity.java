7c20301e1abe32db69f91c9a8474f8e0085c72b5
Gökhan SAVAŞ <Gokhansavasokul@gmail.com>
2021-08-07 23:55:52 +0300

    Basic Calculator

--- app/src/main/java/com/gknsvs/mybasiccalculator/MainActivity.java
+++ app/src/main/java/com/gknsvs/mybasiccalculator/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num1 =findViewById(R.id.TextNum1);
-        num2 =findViewById(R.id.TextNum2);
-        result=findViewById(R.id.textResult);
+        num1 =findViewById(R.id.txtNum1);
+        num2 =findViewById(R.id.txtNum2);
+        result=findViewById(R.id.txtResult);
 
         sharedPreferences=this.getSharedPreferences("com.gknsvs.basiccalculater", Context.MODE_PRIVATE);
 
@@ -65,13 +65,47 @@ public class MainActivity extends AppCompatActivity {
         if(control())
             result.setText(num1.getText().toString()+" / "+num2.getText().toString()+" = "+String.valueOf(Integer.parseInt(num1.getText().toString()) / Integer.parseInt(num2.getText().toString())));
     }
+    public void delete(View view)
+    {
+        AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle("DELETE");
+        alert.setMessage("Are You Sure");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Deleted",Toast.LENGTH_SHORT).show();
+                if ((sharedPreferences.getString("result",null))!=null)
+                    sharedPreferences.edit().remove("result").apply();
+                result.setText("Result");
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Not Delete",Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.show();
+    }
     public void save(View view)
     {
-        sharedPreferences.edit().putString("result",result.getText().toString()).apply();
-    }
-    public void delete(View view) {
-        if ((sharedPreferences.getString("result", null)) != null)
-            sharedPreferences.edit().remove("result").apply();
-        result.setText("Result");
+        AlertDialog.Builder alert=new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are You Sure?");
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"Not Save",Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT).show();
+                sharedPreferences.edit().putString("result",result.getText().toString()).apply();
+            }
+        });
+
+        alert.show();
     }
 }
